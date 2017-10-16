b05cd3dfc5e75dc3749571329cb6147d24cdfe8c
jkesanie <jkesanie@lm5-hulib-1.hulib.helsinki.fi>
2017-10-16 09:20:48 +0300

    mq test

--- attx-broker-tests/src/test/java/org/uh/attx/platform/test/RMLServiceIntegrationTest.java
+++ attx-broker-tests/src/test/java/org/uh/attx/platform/test/RMLServiceIntegrationTest.java
@@ -10,16 +10,28 @@ import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.JsonNode;
 import com.mashape.unirest.http.ObjectMapper;
 import com.mashape.unirest.http.Unirest;
+import com.rabbitmq.client.AMQP;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.Consumer;
+import com.rabbitmq.client.DefaultConsumer;
+import com.rabbitmq.client.Envelope;
 import java.io.File;
 import java.io.IOException;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.TimeUnit;
 import org.apache.commons.io.FileUtils;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.uh.hulib.attx.dev.TestUtils;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.platform.runner.JUnitPlatform;
 import org.junit.runner.RunWith;
+import org.uh.hulib.attx.wc.uv.common.RabbitMQClient;
 import org.uh.hulib.attx.wc.uv.common.pojos.RMLServiceInput;
 import org.uh.hulib.attx.wc.uv.common.pojos.RMLServiceRequest;
 import org.uh.hulib.attx.wc.uv.common.pojos.prov.Context;
@@ -68,10 +80,69 @@ public class RMLServiceIntegrationTest {
 
     }
 
+    @Test 
+    public void testTransformationAMQP() throws Exception {
+        checkHealth();
+        RMLServiceRequest req = getWorkingRequest();
+        com.fasterxml.jackson.databind.ObjectMapper mapper = new com.fasterxml.jackson.databind.ObjectMapper();
+         String reqStr = mapper.writeValueAsString(req);
+        /*
+        RabbitMQClient c = new RabbitMQClient("localhost","user", "password", "provenance.inbox");        
+       
+        System.out.println(reqStr);
+        String respStr = c.sendSyncServiceMessage(reqStr, "rmlservice", 10000);
+        System.out.println("test: " + respStr + "*");
+        assertNotEquals("", respStr);
+*/
+        
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        factory.setUsername("user");
+        factory.setPassword("password");
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+        
+        channel.queueDeclare("test2", true, true, false, null);
+        String rq = "test2";
+        final BlockingQueue<String> response = new ArrayBlockingQueue<String>(1);
+
+        channel.basicConsume(rq, true, new DefaultConsumer(channel) {
+            @Override
+            public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
+                
+//                if (properties.getCorrelationId().equals(corrId)) {
+                    response.offer(new String(body, "UTF-8"));
+//                }
+            }
+        });
+
+        String res = response.poll(5000, TimeUnit.MILLISECONDS);             
+        System.out.println(res);
+    
+        res = response.poll(5000, TimeUnit.MILLISECONDS);             
+        System.out.println(res);
+          
+    }
     @Test
+    @Ignore
     public void testTranformationRest() throws Exception {
         checkHealth();
-
+        RMLServiceRequest req = getWorkingRequest();
+        HttpResponse<JsonNode> resp = Unirest.post(TestUtils.getRMLService() + "/" + API_VERSION + "/transform")
+                .header("accept", "application/json")
+                .header("content-type", "application/json")
+                .body(req)
+                .asJson();
+        
+        assertEquals(200, resp.getStatus());
+        
+        System.out.println(resp.getBody().getObject().toString(2));
+        assertEquals("SUCCESS", resp.getBody().getObject().getJSONObject("payload").get("status"));
+        
+        // TODO: check that content is ok
+    }
+    
+    private RMLServiceRequest getWorkingRequest() throws Exception {
         String mappingStr = FileUtils.readFileToString(new File(getClass().getResource(
                 "/rmlfixtures/etsin-org-map.ttl").toURI()), "UTF-8");
 
@@ -87,19 +158,8 @@ public class RMLServiceIntegrationTest {
         payload.setMapping(mappingStr);
         payload.setInput(inputStr);
         req.setPayload(payload);
-
-        HttpResponse<JsonNode> resp = Unirest.post(TestUtils.getRMLService() + "/" + API_VERSION + "/transform")
-                .header("accept", "application/json")
-                .header("content-type", "application/json")
-                .body(req)
-                .asJson();
         
-        assertEquals(200, resp.getStatus());
-        
-        System.out.println(resp.getBody().getObject().toString(2));
-        assertEquals("SUCCESS", resp.getBody().getObject().getJSONObject("payload").get("status"));
-        
-        // TODO: check that content is ok
+        return req;
     }
 
     private Provenance getProvenance() {
